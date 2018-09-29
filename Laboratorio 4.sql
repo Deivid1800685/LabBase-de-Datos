66b52e9ddb7ec78d7f8ec36ab2bb71d2db6d0001
Deivid1800685 <42881933+Deivid1800685@users.noreply.github.com>
2018-09-28 22:23:09 -0500

    Laboratorio 4 insert, update y delete

---
+++ Laboratorio 4.sql
@@ -0,0 +1,309 @@
+use Videos3
+
+insert into Forma_Pago (vForma_Pago)
+values ('Efectivo')
+insert into Forma_Pago (vForma_Pago)
+values ('Tarjeta de credito')
+insert into Forma_Pago (vForma_Pago)
+values ('Efectivo')
+insert into Forma_Pago (vForma_Pago)
+values ('Tarjeta de credito')
+insert into Forma_Pago (vForma_Pago)
+values ('Efectivo')
+insert into Forma_Pago (vForma_Pago)
+values ('Tarjeta de credito')
+insert into Forma_Pago (vForma_Pago)
+values ('Tarjeta de credito')
+insert into Forma_Pago (vForma_Pago)
+values ('Efectivo')
+insert into Forma_Pago (vForma_Pago)
+values ('Efectivo')
+insert into Forma_Pago (vForma_Pago)
+values ('Efectivo')
+insert into Forma_Pago (vForma_Pago)
+values ('Tarjeta de credito')
+insert into Forma_Pago (vForma_Pago)
+values ('Tarjeta de credito')
+insert into Forma_Pago (vForma_Pago)
+values ('Efectivo')
+insert into Forma_Pago (vForma_Pago)
+values ('Efectivo')
+select * from Forma_Pago
+update Forma_Pago
+set vForma_Pago = 'Tarjeta de credito'
+where iID_Pago = 90
+update Forma_Pago
+set vForma_Pago = 'Tarjeta de credito'
+where iID_Pago = 91
+update Forma_Pago
+set vForma_Pago = 'Efectivo'
+where iID_Pago = 92
+update Forma_Pago
+set vForma_Pago = 'Tarjeta de credito'
+where iID_Pago = 93
+update Forma_Pago
+set vForma_Pago = 'Tarjeta de credito'
+where iID_Pago = 95
+delete from Forma_Pago 
+where iID_Pago = 117
+delete from Forma_Pago 
+where iID_Pago = 116
+delete from Forma_Pago 
+where iID_Pago = 115
+delete from Forma_Pago 
+where iID_Pago = 114
+delete from Forma_Pago 
+where iID_Pago = 113
+
+insert into Pelicula (vNombre_Peli,iAño_Estreno,mPrecio,fPuntuaje,vDuración,tDescripción,tCantidad,iID_Genero,iID_Actor,iID_Formato,iID_Clasificacion,iID_Calidad,iID_Idioma,iID_Resolucion)
+values ('Avengers infinity war',2018,120,4.75,'Dos horas','Thanos ya viene',5,40,30,20,10,70,60,50)
+insert into Pelicula (vNombre_Peli,iAño_Estreno,mPrecio,fPuntuaje,vDuración,tDescripción,tCantidad,iID_Genero,iID_Actor,iID_Formato,iID_Clasificacion,iID_Calidad,iID_Idioma,iID_Resolucion)
+values ('Avengers la era de ultron',2018,120,4.75,'Dos horas','Una pelicula de super heroes',5,40,30,20,10,70,60,50)
+insert into Pelicula (vNombre_Peli,iAño_Estreno,mPrecio,fPuntuaje,vDuración,tDescripción,tCantidad,iID_Genero,iID_Actor,iID_Formato,iID_Clasificacion,iID_Calidad,iID_Idioma,iID_Resolucion)
+values ('Avengers',2018,120,4.75,'Dos horas','se reunen',5,40,30,20,10,70,60,50)
+insert into Pelicula (vNombre_Peli,iAño_Estreno,mPrecio,fPuntuaje,vDuración,tDescripción,tCantidad,iID_Genero,iID_Actor,iID_Formato,iID_Clasificacion,iID_Calidad,iID_Idioma,iID_Resolucion)
+values ('It',2015,120,4.75,'Dos horas','6 niños vs un payaso',5,44,30,20,10,70,60,50)
+insert into Pelicula (vNombre_Peli,iAño_Estreno,mPrecio,fPuntuaje,vDuración,tDescripción,tCantidad,iID_Genero,iID_Actor,iID_Formato,iID_Clasificacion,iID_Calidad,iID_Idioma,iID_Resolucion)
+values ('Jack reacher',2012,120,4.75,'Una hora y media','Hombre de accion',5,41,34,20,10,70,60,50)
+insert into Pelicula (vNombre_Peli,iAño_Estreno,mPrecio,fPuntuaje,vDuración,tDescripción,tCantidad,iID_Genero,iID_Actor,iID_Formato,iID_Clasificacion,iID_Calidad,iID_Idioma,iID_Resolucion)
+values ('Mulan',1997,120,4.75,'Una hora y media','el patriarcado',5,42,30,20,10,70,60,50)
+insert into Pelicula (vNombre_Peli,iAño_Estreno,mPrecio,fPuntuaje,vDuración,tDescripción,tCantidad,iID_Genero,iID_Actor,iID_Formato,iID_Clasificacion,iID_Calidad,iID_Idioma,iID_Resolucion)
+values ('Spiderman',2000,120,4.75,'Dos hora quince minutos','Una pelicula de peter parker',5,40,35,20,10,70,60,50)
+insert into Pelicula (vNombre_Peli,iAño_Estreno,mPrecio,fPuntuaje,vDuración,tDescripción,tCantidad,iID_Genero,iID_Actor,iID_Formato,iID_Clasificacion,iID_Calidad,iID_Idioma,iID_Resolucion)
+values ('Spiderman 2',2004,120,4.75,'Dos hora quince minutos','Una pelicula de peter parker 2',5,40,35,20,10,70,60,50)
+insert into Pelicula (vNombre_Peli,iAño_Estreno,mPrecio,fPuntuaje,vDuración,tDescripción,tCantidad,iID_Genero,iID_Actor,iID_Formato,iID_Clasificacion,iID_Calidad,iID_Idioma,iID_Resolucion)
+values ('Spiderman 3',2007,120,4.75,'Dos hora quince minutos','Una pelicula de peter parker 3',5,40,35,20,10,70,60,50)
+insert into Pelicula (vNombre_Peli,iAño_Estreno,mPrecio,fPuntuaje,vDuración,tDescripción,tCantidad,iID_Genero,iID_Actor,iID_Formato,iID_Clasificacion,iID_Calidad,iID_Idioma,iID_Resolucion)
+values ('Jack reacher 2',2017,120,4.75,'Una hora y media','Hombre de accion 2',5,41,34,20,10,70,60,50)
+insert into Pelicula (vNombre_Peli,iAño_Estreno,mPrecio,fPuntuaje,vDuración,tDescripción,tCantidad,iID_Genero,iID_Actor,iID_Formato,iID_Clasificacion,iID_Calidad,iID_Idioma,iID_Resolucion)
+values ('Jurassic Wolrd',2018,120,4.75,'Dos horas','Dinosaurios',5,41,33,20,10,70,60,50)
+insert into Pelicula (vNombre_Peli,iAño_Estreno,mPrecio,fPuntuaje,vDuración,tDescripción,tCantidad,iID_Genero,iID_Actor,iID_Formato,iID_Clasificacion,iID_Calidad,iID_Idioma,iID_Resolucion)
+values ('Iron Man',2008,120,4.75,'Dos horas','Millonario playboy filantropo',5,41,30,20,10,70,60,50)
+insert into Pelicula (vNombre_Peli,iAño_Estreno,mPrecio,fPuntuaje,vDuración,tDescripción,tCantidad,iID_Genero,iID_Actor,iID_Formato,iID_Clasificacion,iID_Calidad,iID_Idioma,iID_Resolucion)
+values ('Iron Man 2',2012,120,4.75,'Dos horas','Millonario playboy filantropo',5,41,30,20,10,70,60,50)
+insert into Pelicula (vNombre_Peli,iAño_Estreno,mPrecio,fPuntuaje,vDuración,tDescripción,tCantidad,iID_Genero,iID_Actor,iID_Formato,iID_Clasificacion,iID_Calidad,iID_Idioma,iID_Resolucion)
+values ('Iron Man 3',2015,120,4.75,'Dos horas','Millonario playboy filantropo',5,41,30,20,10,70,60,50)
+insert into Pelicula (vNombre_Peli,iAño_Estreno,mPrecio,fPuntuaje,vDuración,tDescripción,tCantidad,iID_Genero,iID_Actor,iID_Formato,iID_Clasificacion,iID_Calidad,iID_Idioma,iID_Resolucion)
+values ('Capitan America',2010,120,4.75,'Dos horas','Un soldado que viene de otra era',5,41,32,20,10,70,60,50)
+insert into Pelicula (vNombre_Peli,iAño_Estreno,mPrecio,fPuntuaje,vDuración,tDescripción,tCantidad,iID_Genero,iID_Actor,iID_Formato,iID_Clasificacion,iID_Calidad,iID_Idioma,iID_Resolucion)
+values ('Capitan America 2',2013,120,4.75,'Dos horas','Un soldado que viene de otra era',5,41,32,20,10,70,60,50)
+insert into Pelicula (vNombre_Peli,iAño_Estreno,mPrecio,fPuntuaje,vDuración,tDescripción,tCantidad,iID_Genero,iID_Actor,iID_Formato,iID_Clasificacion,iID_Calidad,iID_Idioma,iID_Resolucion)
+values ('Capitan America 3',2016,120,4.75,'Dos horas','Civil war',5,41,32,20,10,70,60,50)
+insert into Pelicula (vNombre_Peli,iAño_Estreno,mPrecio,fPuntuaje,vDuración,tDescripción,tCantidad,iID_Genero,iID_Actor,iID_Formato,iID_Clasificacion,iID_Calidad,iID_Idioma,iID_Resolucion)
+values ('Spiderman Homecomming',2010,120,4.75,'Dos horas','Spiderman y Iron Man',5,41,31,20,10,70,60,50)
+
+update Pelicula
+set iAño_Estreno = '2011'
+where vNombre_Peli = 'Capitan America'
+update Pelicula
+set iAño_Estreno = '2009'
+where vNombre_Peli = 'Iron Man'
+update Pelicula
+set iAño_Estreno = '2014'
+where vNombre_Peli = 'Capitan America 2'
+update Pelicula
+set iAño_Estreno = '2010'
+where vNombre_Peli = 'Iron Man 2'
+update Pelicula
+set mPrecio = '45'
+where vNombre_Peli = 'Spiderman'
+insert into Pelicula (vNombre_Peli,iAño_Estreno,mPrecio,fPuntuaje,vDuración,tDescripción,tCantidad,iID_Genero,iID_Actor,iID_Formato,iID_Clasificacion,iID_Calidad,iID_Idioma,iID_Resolucion)
+values ('Iron Man 3',2015,120,4.75,'Dos horas','Millonario playboy filantropo',5,41,30,20,10,70,60,50)
+insert into Pelicula (vNombre_Peli,iAño_Estreno,mPrecio,fPuntuaje,vDuración,tDescripción,tCantidad,iID_Genero,iID_Actor,iID_Formato,iID_Clasificacion,iID_Calidad,iID_Idioma,iID_Resolucion)
+values ('Capitan America',2010,120,4.75,'Dos horas','Un soldado que viene de otra era',5,41,32,20,10,70,60,50)
+insert into Pelicula (vNombre_Peli,iAño_Estreno,mPrecio,fPuntuaje,vDuración,tDescripción,tCantidad,iID_Genero,iID_Actor,iID_Formato,iID_Clasificacion,iID_Calidad,iID_Idioma,iID_Resolucion)
+values ('Capitan America 2',2013,120,4.75,'Dos horas','Un soldado que viene de otra era',5,41,32,20,10,70,60,50)
+insert into Pelicula (vNombre_Peli,iAño_Estreno,mPrecio,fPuntuaje,vDuración,tDescripción,tCantidad,iID_Genero,iID_Actor,iID_Formato,iID_Clasificacion,iID_Calidad,iID_Idioma,iID_Resolucion)
+values ('Capitan America 3',2016,120,4.75,'Dos horas','Civil war',5,41,32,20,10,70,60,50)
+insert into Pelicula (vNombre_Peli,iAño_Estreno,mPrecio,fPuntuaje,vDuración,tDescripción,tCantidad,iID_Genero,iID_Actor,iID_Formato,iID_Clasificacion,iID_Calidad,iID_Idioma,iID_Resolucion)
+values ('Spiderman Homecomming',2010,120,4.75,'Dos horas','Spiderman y Iron Man',5,41,31,20,10,70,60,50)
+delete from Pelicula 
+where iID_Pelicula = 98
+delete from Pelicula 
+where iID_Pelicula = 99
+delete from Pelicula 
+where iID_Pelicula = 100
+delete from Pelicula 
+where iID_Pelicula = 101
+delete from Pelicula 
+where iID_Pelicula = 102
+select * from Pelicula
+
+insert into Cliente (vNombre_Cliente,vApellidoP_Cliente,vApellidoM_Cliente,vContacto,iID_Pago,iID_Domicilio,iID_ClienDistinguido)
+values('David','Avila','Olmeda','M',90,160,170)
+insert into Cliente (vNombre_Cliente,vApellidoP_Cliente,vApellidoM_Cliente,dFecha_Nac,vContacto,iID_Pago,iID_Domicilio,iID_ClienDistinguido)
+values('Pablo','Lopez','Sanchez','2000/05/23','M',90,161,171)
+insert into Cliente (vNombre_Cliente,vApellidoP_Cliente,vApellidoM_Cliente,dFecha_Nac,vContacto,iID_Pago,iID_Domicilio,iID_ClienDistinguido)
+values('Alejandra','Garcia','Sanchez','1999/07/19','F',91,162,172)
+insert into Cliente (vNombre_Cliente,vApellidoP_Cliente,vApellidoM_Cliente,dFecha_Nac,vContacto,iID_Pago,iID_Domicilio,iID_ClienDistinguido)
+values('Juan','Bueno','Lara','1999/07/03','M',91,163,173)
+insert into Cliente (vNombre_Cliente,vApellidoP_Cliente,vApellidoM_Cliente,dFecha_Nac,vContacto,iID_Pago,iID_Domicilio,iID_ClienDistinguido)
+values('David','Perez','Darif','2001/12/21','M',91,164,174)
+insert into Cliente (vNombre_Cliente,vApellidoP_Cliente,vApellidoM_Cliente,dFecha_Nac,vContacto,iID_Pago,iID_Domicilio,iID_ClienDistinguido)
+values('Roberto','Pesina','Juarez','1998/09/15','M',90,165,175)
+insert into Cliente (vNombre_Cliente,vApellidoP_Cliente,vApellidoM_Cliente,dFecha_Nac,vContacto,iID_Pago,iID_Domicilio,iID_ClienDistinguido)
+values('Tadeo','Lopez','Martinez','1998/04/24','M',90,166,176)
+insert into Cliente (vNombre_Cliente,vApellidoP_Cliente,vApellidoM_Cliente,dFecha_Nac,vContacto,iID_Pago,iID_Domicilio,iID_ClienDistinguido)
+values('Paulina','Vazquez','Atxutegi','1998/09/30','F',91,167,177)
+insert into Cliente (vNombre_Cliente,vApellidoP_Cliente,vApellidoM_Cliente,dFecha_Nac,vContacto,iID_Pago,iID_Domicilio,iID_ClienDistinguido)
+values('Andrea','Vazquez','Atxutegi','1998/09/30','F',91,168,178)
+insert into Cliente (vNombre_Cliente,vApellidoP_Cliente,vApellidoM_Cliente,dFecha_Nac,vContacto,iID_Pago,iID_Domicilio,iID_ClienDistinguido)
+values('Mario','Gutierrez','Ezpinoza','1998/01/02','M',91,169,179)
+insert into Cliente (vNombre_Cliente,vApellidoP_Cliente,vApellidoM_Cliente,dFecha_Nac,vContacto,iID_Pago,iID_Domicilio,iID_ClienDistinguido)
+values('Erick','Martinez','de la Paz','2000/03/28','M',90,170,180)
+insert into Cliente (vNombre_Cliente,vApellidoP_Cliente,vApellidoM_Cliente,dFecha_Nac,vContacto,iID_Pago,iID_Domicilio,iID_ClienDistinguido)
+values('Horacio','Hernandez','de Dios','1999/05/07','M',90,171,181)
+insert into Cliente (vNombre_Cliente,vApellidoP_Cliente,vApellidoM_Cliente,dFecha_Nac,vContacto,iID_Pago,iID_Domicilio,iID_ClienDistinguido)
+values('Rodrigo','Bravo','Ramos','1999/10/12','M',91,172,182)
+insert into Cliente (vNombre_Cliente,vApellidoP_Cliente,vApellidoM_Cliente,dFecha_Nac,vContacto,iID_Pago,iID_Domicilio,iID_ClienDistinguido)
+values('Zabdiel','Diaz','Cerda','1999/11/18','M',91,173,183)
+insert into Cliente (vNombre_Cliente,vApellidoP_Cliente,vApellidoM_Cliente,dFecha_Nac,vContacto,iID_Pago,iID_Domicilio,iID_ClienDistinguido)
+values('Jorge','Garcia','Marin','2000/02/20','M',90,174,184)
+
+select * from Cliente
+update Cliente
+set dFecha_Nac = '2000/04/24' 
+where iID_Cliente = '100'
+update Cliente
+set dFecha_Nac = '1998/03/28' 
+where iID_Cliente = '101'
+update Cliente
+set dFecha_Nac = '2000/08/24' 
+where iID_Cliente = '102'
+update Cliente
+set dFecha_Nac = '1997/06/07' 
+where iID_Cliente = '103'
+update Cliente
+set dFecha_Nac = '1999/10/14' 
+where iID_Cliente = '104'
+delete from Cliente 
+where iID_Cliente = 100
+delete from Cliente 
+where iID_Cliente = 101
+delete from Cliente 
+where iID_Cliente = 102
+delete from Cliente 
+where iID_Cliente = 103
+delete from Cliente 
+where iID_Cliente = 104
+insert into Formato(vFormato)
+values('Blu-ray')
+insert into Formato(vFormato)
+values('MP4')
+insert into Formato(vFormato)
+values('AVI')
+insert into Formato(vFormato)
+values('H264')
+insert into Formato(vFormato)
+values('MPG')
+insert into Formato(vFormato)
+values('WMV')
+insert into Formato(vFormato)
+values('MOV')
+insert into Formato(vFormato)
+values('MKV')
+insert into Formato(vFormato)
+values('DIVX')
+insert into Formato(vFormato)
+values('XVID')
+insert into Formato(vFormato)
+values('FLV')
+insert into Formato(vFormato)
+values('3GP')
+insert into Formato(vFormato)
+values('DVD')
+insert into Formato(vFormato)
+values('VHS')
+insert into Formato(vFormato)
+values('Real Media')
+select * from Formato
+update Formato
+set vFormato = 'DVD' 
+where iID_Formato = '20'
+update Formato
+set vFormato = 'Blu eay' 
+where iID_Formato = '20'
+update Formato
+set vFormato = 'Blu ray' 
+where iID_Formato = '20'
+update Formato
+set vFormato = 'PSV' 
+where iID_Formato = '21'
+update Formato
+set vFormato = 'SVG' 
+where iID_Formato = '22'
+delete from Formato 
+where iID_Formato = '37'
+delete from Formato 
+where iID_Formato = '26'
+delete from Formato 
+where iID_Formato = '28'
+delete from Formato 
+where iID_Formato = '29'
+delete from Formato 
+where iID_Formato = '25'
+insert into Resolucion(vResolucion)
+values('CGA')
+insert into Resolucion(vResolucion)
+values('QVGA')
+insert into Resolucion(vResolucion)
+values('B&W Macintosh/Macintosh LC')
+insert into Resolucion(vResolucion)
+values('EGA')
+insert into Resolucion(vResolucion)
+values('VGA')
+insert into Resolucion(vResolucion)
+values('HGC')
+insert into Resolucion(vResolucion)
+values('MDA')
+insert into Resolucion(vResolucion)
+values('Apple lisa')
+insert into Resolucion(vResolucion)
+values('SVGA')
+insert into Resolucion(vResolucion)
+values('WVGA')
+insert into Resolucion(vResolucion)
+values('XGA')
+insert into Resolucion(vResolucion)
+values('WXGA')
+insert into Resolucion(vResolucion)
+values('SXGA')
+insert into Resolucion(vResolucion)
+values('Full HD')
+insert into Resolucion(vResolucion)
+values('Retina Display')
+insert into Resolucion(vResolucion)
+values('4k Cinema')
+update Resolucion
+set vResolucion = 'A' 
+where iID_Resolucion = '66'
+update Resolucion
+set vResolucion = 'WUXGA' 
+where iID_Resolucion = '66'
+update Resolucion
+set vResolucion = '4k Cinema' 
+where iID_Resolucion = '66'
+update Resolucion
+set vResolucion = '4k' 
+where iID_Resolucion = '66'
+insert into Resolucion(vResolucion)
+values('DWXGA')
+insert into Resolucion(vResolucion)
+values('DSXGA')
+insert into Resolucion(vResolucion)
+values('DFull HD')
+insert into Resolucion(vResolucion)
+values('DRetina Display')
+insert into Resolucion(vResolucion)
+values('D4k Cinema')
+delete from Resolucion
+where iID_Resolucion = '67'
+delete from Resolucion
+where iID_Resolucion = '68'
+delete from Resolucion
+where iID_Resolucion = '69'
+delete from Resolucion
+where iID_Resolucion = '70'
+delete from Resolucion
+where iID_Resolucion = '71'
+select * from Resolucion
